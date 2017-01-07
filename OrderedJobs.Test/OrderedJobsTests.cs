f9b2bde8d142caa675b6b74af8efc9b4742feea7
Wesley Duncan <duncan.wesley@gmail.com>
2017-01-07 18:46:58 -0500

    orders multiple jobs, multiple dependencies

--- OrderedJobs.Test/OrderedJobsTests.cs
+++ OrderedJobs.Test/OrderedJobsTests.cs
@@ -34,5 +34,22 @@ namespace OrderedJobs.Test
       Assert.That(indexOfB, Is.GreaterThan(indexOfC));
       Assert.That(orderedJobs.Length, Is.EqualTo(orderedJobs.ToCharArray().Distinct().Count()));
     }
+
+    [Test]
+    public void MultipleJobsMultipleDependenciesTest()
+    {
+      var orderedJobs = JobOrderer.Order("a-|b-c|c-f|d-a|e-b|f-");
+      var indexOfA = orderedJobs.IndexOf("a");
+      var indexOfB = orderedJobs.IndexOf("b");
+      var indexOfC = orderedJobs.IndexOf("c");
+      var indexOfD = orderedJobs.IndexOf("d");
+      var indexOfE = orderedJobs.IndexOf("e");
+      var indexOfF = orderedJobs.IndexOf("f");
+      Assert.That(indexOfB, Is.GreaterThan(indexOfC));
+      Assert.That(indexOfC, Is.GreaterThan(indexOfF));
+      Assert.That(indexOfD, Is.GreaterThan(indexOfA));
+      Assert.That(indexOfE, Is.GreaterThan(indexOfB));
+      Assert.That(orderedJobs.Length, Is.EqualTo(orderedJobs.ToCharArray().Distinct().Count()));
+    }
   }
 }
--- src/OrderedJobs.Domain/JobOrderer.cs
+++ src/OrderedJobs.Domain/JobOrderer.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace OrderedJobs.Domain
 {
@@ -9,18 +10,29 @@ namespace OrderedJobs.Domain
       if (jobs.Length == 0) return "";
       var splitJobs = jobs.Split('|');
 
-      var orderedJobs = splitJobs.Where(job => job[job.Length - 1] == '-')
-        .Aggregate("", (current, job) => current + job[0]);
-      var jobsToAdd = splitJobs.Where(job => !orderedJobs.Contains(job[0])).ToList();
+      var orderedJobs = GetJobsWithNoDependencies(splitJobs);
+      var jobsToAdd = GetJobsToAdd(splitJobs, orderedJobs);
       while (jobsToAdd.Any())
       {
-        foreach (var jobToAdd in jobsToAdd)
-          if (orderedJobs.Contains(jobToAdd[2]))
-            orderedJobs += jobToAdd[0];
-        jobsToAdd = splitJobs.Where(job => !orderedJobs.Contains(job[0])).ToList();
+        orderedJobs = jobsToAdd
+          .Where(jobToAdd => orderedJobs.Contains(jobToAdd[2]))
+          .Aggregate(orderedJobs, (current, job) => current + job[0]);
+        jobsToAdd = GetJobsToAdd(splitJobs, orderedJobs);
       }
 
       return orderedJobs;
+    }
+
+    private static List<string> GetJobsToAdd(string[] splitJobs, string orderedJobs)
+    {
+      return splitJobs.Where(job => !orderedJobs.Contains(job[0])).ToList();
+    }
+
+    private static string GetJobsWithNoDependencies(string[] splitJobs)
+    {
+      return splitJobs
+        .Where(job => job[job.Length - 1] == '-')
+        .Aggregate("", (current, job) => current + job[0]);
     }
   }
 }
