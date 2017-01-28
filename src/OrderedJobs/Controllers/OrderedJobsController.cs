aa1fab6cd5f190e9b68638077a85773cfdb48218
Wesley Duncan <duncan.wesley@gmail.com>
2017-01-28 13:09:13 -0500

    remove unneeded code in orderJobs controller

--- src/OrderedJobs/Controllers/OrderedJobsController.cs
+++ src/OrderedJobs/Controllers/OrderedJobsController.cs
@@ -12,36 +12,11 @@ namespace OrderedJobs.Controllers
     {
       _jobOrderer = jobOrderer;
     }
-    // GET api/values
-    [HttpGet]
-    public string Get()
-    {
-      return "value1";
-    }
 
-    // GET api/values/5
     [HttpGet("{dependencies}")]
     public string Get(string dependencies)
     {
       return _jobOrderer.Order(dependencies);
-    }
-
-    // POST api/values
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
     }
   }
 }
